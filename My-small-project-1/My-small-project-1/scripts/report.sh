cdbaaa3e72cfa3c0510c2446753b47be238e20b0
naya <nayaalmustafa2@gmail.com>
2026-06-11 12:41:45 +0300

    Update report.sh

--- My-small-project-1/My-small-project-1/scripts/report.sh
+++ My-small-project-1/My-small-project-1/scripts/report.sh
@@ -4,8 +4,8 @@ mkdir -p notes
 
 REPORT_FILE="notes/report-$(date +%F).txt"
 
-echo "اسم المستخدم: $(whoami)" > "$REPORT_FILE"
+echo "اسم المستخدم: $USER" > "$REPORT_FILE"
 echo "المسار الحالي: $(pwd)" >> "$REPORT_FILE"
-echo "عدد الملفات داخل data: $(find data -type f | wc -l)" >> "$REPORT_FILE"
+echo "عدد الملفات داخل المشروع: $(find . -type f | wc -l)" >> "$REPORT_FILE"
 
 echo "تم إنشاء التقرير بنجاح"
